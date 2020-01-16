a1229b1b7ebe8d62b2fcc3883b975af76b37d473
Krolos Abdou <KrolosA@hotmail.com>
2020-01-16 13:43:43 +0100

    Average calculate

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,5 +1,8 @@
 from grid import gridplotter
+from House import House
 import json
+from loadfiles import loadhouse
+from helpers import manhatten_distance
 
 def pathing_test():
     with open('../Scores/wijk3_score_advanced1.txt') as json_file:
@@ -11,4 +14,29 @@ def test_modulo():
         print(j % 1000)
         if j % 1000 == 0:
             print('HET WERKT')
-pathing_test()
+
+def calc_average(path):
+    houseslist = loadhouse(path)
+
+    houses = []
+    for house in houseslist:
+
+        # clean up data
+        temp = house.replace(' ', '').split(',')
+        temp = [float(i) for i in temp]
+        houses.append(House((temp[0], temp[1]), temp[2]))
+
+    average = 0
+    for house in houses:
+        shortest = 100
+        for neighbor in houses:
+            if neighbor == house:
+                continue
+            distance = manhatten_distance(house.coord, neighbor.coord)
+            if distance < shortest:
+                shortest = distance
+        average += shortest
+
+    print(average/150, average)
+
+calc_average('../Data/wijk2_huizen.csv')

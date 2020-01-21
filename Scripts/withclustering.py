f3ad933f0bb31958bbb91d0dd751888a766a0744
Jonas Heller <JonasHeller@wcw-stud-145-109-46-52.wireless.uva.nl>
2020-01-21 12:23:40 +0100

    Added bargraph

--- Scripts/withclustering.py
+++ Scripts/withclustering.py
@@ -4,7 +4,7 @@ from loadfiles import loadbattery, loadhouse
 from makeitjson import makejson
 import pprint as pp
 from helpers import get_all_cables, get_houses_left, averagex_andy, get_outliers, manhatten_distance, connect_houses, \
-    connect_houses_from_houses, save_highscore
+    connect_houses_from_houses, save_highscore, scores_plot
 from grid import gridplotter
 import random
 import json
@@ -48,7 +48,7 @@ for i in range(100000):
         batteries[battery].calculate_distances(houses)
 
     #batteries, houses, houses_left = connect_houses(batteries, houses)
-    batteries, houses_left = connect_houses_from_houses(batteries, houses)
+    batteries, houses_left = connect_houses(batteries, houses)
     if len(houses_left) > 0:
         continue
 
@@ -75,3 +75,5 @@ print(sorted(scores))
 gridplotter(highest)
 
 save_highscore(highscore_file, highest)
+
+scores_plot(scores)
